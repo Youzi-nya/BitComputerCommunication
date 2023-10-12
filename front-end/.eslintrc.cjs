745cb1a45eee8cbc730c4bda2728d5375d63736b
apprehen <3514392356@qq.com>
2023-10-12 19:47:08 +0800

    fix(front-end): add the stand pack

--- front-end/.eslintrc.cjs
+++ front-end/.eslintrc.cjs
@@ -1,21 +1,40 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2021: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    "plugin:perttier/recommended",
-    "standard-with-typescript"
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  plugins: ['react','@typescript-eslint','perttire'],
-  rules: {
-    quotes: ['error', 'single'],
-    semi: ['error', 'never'],
-    'comma-dangle': ['error', 'never'],
-    'prettier/prettier': 'error',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-explicit-any': ['off']
-}
-}
+    env: {
+      browser: true,
+      es2021: true,
+      node: true
+    },
+  
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ],
+    overrides: [
+      {
+        env: {
+          node: true
+        },
+        files: ['.eslintrc.{js,cjs}'],
+        parserOptions: {
+          sourceType: 'script'
+        }
+      }
+    ],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      project: './tsconfig.eslint.json'
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': ['off']
+    }
+  }
+  
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,22 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <div></div>
       <h1>Vite + React</h1>
+      <a href="11"></a>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,5 +22,4 @@ function App() {
     </>
   )
 }
-
 export default App
